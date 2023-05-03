dd0eaf035615919dc66168d41fc19bfc9cd729f6
Sumet Jutajan <sumet.jutha@gmail.com>
2023-05-04 02:16:04 +0700

    Update try_2.py

--- try_2.py
+++ try_2.py
@@ -1,16 +1,41 @@
 import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+
 
 listener = sr.Recognizer()
 
-try:
-    with sr.Microphone() as origin:
-        print("Listening ")
-        speech = listener.listen(origin)
-        instruction = listener.recognize_goggle(speech)
-        instruction = instruction.lower()
-        if "Jarvis" in instruction:
-            print(instruction)
-        print(instruction)
+machine = pyttsx3.init()
+
+def talk(text):
+    machine.say(text)
+    
+machine.runAndWait()
+
+def input_instruction():
+    global instruction
+    try:
+        with sr.Microphone() as origin:
+            print("Listening ")
+            speech = listener.listen(origin)
+            instruction = listener.recognize_goggle(speech)
+            instruction = instruction.lower()
+            if "Jarvis" in instruction:
+                print(instruction)
+    except:
+        pass
+    return instruction
+
+def play_Jarvis():
+    instruction = input_instruction
+    print(instruction)
+    if "play" in instruction:
+        song = instruction.replace('play', "")
+        talk("playing" + song)
+        pywhatkit.playonyt(song)
         
-except:
-    pass
+    elif 'time' in instruction:
+        time = datetime.datetime.now().strftime
+        
+play_Jarvis()
